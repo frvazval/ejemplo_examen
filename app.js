4082b5ecf005ef9190ab150f2e5358d4cbe3f43d
Fvazquez <foapgenerico@365.stucom.com>
2025-07-15 18:21:39 +0200

    stucom

---
+++ app.js
@@ -0,0 +1,13 @@
+// Importar las dependencias
+import express from "express";
+import {connection} from "./mysql/mysql.js";
+
+// Obtener el puerto del servidor
+process.loadEnvFile();
+const PORT = process.env.PORT || 5000;
+console.log(PORT);
+
+// Crear la aplicación
+const app = express();
+
+// 
