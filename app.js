cbe90e9e5824a8cc6e5576fcba34ce78d98fe99b
Fvazquez <foapgenerico@365.stucom.com>
2025-07-15 19:30:44 +0200

    stucom

--- app.js
+++ app.js
@@ -15,5 +15,23 @@ app.get("/", (req, res) => {
     res.send("Aqui irá la página inicial");
 });
 
+app.get("/alumno", (req, res) => {
+    const query = "SELECT * FROM alumno";
+    connection.query(query, (err, result, fields) => {
+        if(err) throw err;
+        // console.log(result);
+        res.json(result);
+    })
+});
+
+app.get("/alumno/:valor", (req, res) => {
+    const query = `SELECT * FROM alumno WHERE apellido1 LIKE "${req.params.valor}%" ORDER BY apellido1, apellido2`
+    connection.query(query, (err, result, fields) => {
+        if(err) throw err;
+        // console.log(result);
+        res.json(result);
+    })
+});
+
 // 
 app.listen(PORT, () => console.log(`Servidor abierto en http://127.0.0.1:${PORT}`));
---
+++ sql/consultas.sql
@@ -0,0 +1,5 @@
+use facultad;
+
+SELECT * FROM alumno;
+
+SELECT * FROM alumno WHERE apellido1 LIKE "G%" ORDER BY apellido1, apellido2;
