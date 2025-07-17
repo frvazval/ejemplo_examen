e51cf83dbf4253a7d214f43e346483e66814fdb7
Fvazquez <foapgenerico@365.stucom.com>
2025-07-17 18:36:10 +0200

    stucom

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -4,3 +4,12 @@ SELECT * FROM alumno;
 
 SELECT * FROM alumno WHERE apellido1 LIKE "G%" ORDER BY apellido1, apellido2;
 
+SELECT a.nombre
+FROM profesor p 
+NATURAL JOIN impartir i 
+INNER JOIN asignatura a ON i.idAsignatura = a.idAsignatura
+WHERE p.nombre = "David" AND p.apellido1 = "Serna";
+
+
+
+ 
